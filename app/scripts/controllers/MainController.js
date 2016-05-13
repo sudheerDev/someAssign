ea69343ec136234cf1df96c93fd5f19832c60f6a
SudheerDev <sudheer.105@gmail.com>
2016-05-13 18:28:37 +0530

    first commit

---
+++ app/scripts/controllers/MainController.js
@@ -0,0 +1,50 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name codeAssignApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the codeAssignApp
+ */
+angular.module('codeAssignApp')
+  .controller('MainCtrl', ['$scope', 'UserInfoFactory',function ($scope, UserInfoFactory) {
+
+    var index = 0;
+    $scope.init = function () {
+      $scope.user = {
+        'name': null
+      };
+
+      $scope.pageResults = [];
+      UserInfoFactory.getJson().then(function (response) {
+        $scope.sortedUserInfo = _.sortBy(response, 'created_at');
+        $scope.appendResults();
+      });
+    };
+
+    $scope.appendResults = function () {
+      $scope.pageResults = $scope.pageResults.concat($scope.sortedUserInfo[0].slice(index, index+50));
+      console.log($scope.pageResults, $scope.sortedUserInfo);
+      index = index + 50;
+    };
+
+    $scope.searchChange = function () {
+      var searchString = $scope.user.name;
+      console.log($scope.user.name === '');
+      if ($scope.user.name === '') {
+        $scope.pageResults = [];
+        index = 0;
+        $scope.appendResults();
+      } else {
+        $scope.pageResults = _.filter($scope.sortedUserInfo[0], function(user){
+          if ( !_.isUndefined(user.name) && user.name.toLowerCase().indexOf(searchString.toLowerCase()) !== -1) {
+            return user;
+          }
+        });
+      }
+
+    };
+
+    $scope.init();
+  }]);
---
+++ app/scripts/factories/MainFactory.js
@@ -0,0 +1,19 @@
+angular.module('codeAssignApp')
+  .factory('UserInfoFactory', ['$http','$q', function ($http, $q) {
+    var userInfo = {};
+    var baseURL = "/json";
+
+  userInfo.getJson = function (id) {
+    return $http({
+       method: 'GET',
+       url: baseURL+ "/userInfo.json"
+     }).then(function (response) {
+       return $q.when(response.data);
+     }, function (responseError) {
+       console.log('error');
+       return $q.reject(responseError);
+     });
+   }
+
+    return userInfo;
+ }]);
